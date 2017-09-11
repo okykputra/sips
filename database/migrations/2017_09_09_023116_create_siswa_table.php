c921aab85fc9f5856fef95ff2a3018c4f4507df0
alifeko404 <alifeko404@gmail.com>
2017-09-11 14:25:19 +0700

    bismillah

---
+++ database/migrations/2017_09_09_023116_create_siswa_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_siswa');
+            $table->integer('kelas_id');
+            $table->string('absen');
+            $table->string('jenis_kelamin');
+            $table->text('alamat');
+            $table->string('nomer_telpon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('siswa');
+    }
+}
