04916baa5eb6142e95f2575c799a64f88cf6b4d0
yuzron <yuzyus267@gmail.com>
2017-09-12 14:44:39 +0700

    yep

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <a href=#></a><b>SMKN 1 Dlanggu</b></a>
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2017 <a href="#">@Rpl_Smakgu</a>.</strong>
+    <strong>Copyright &copy;2017 <a href="#">@Rpl_Smakgu</a>.</strong>
 </footer>
