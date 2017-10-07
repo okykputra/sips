ace09f050d26cd4d1cddf8a8d3e398223f38a6f4
okykputra <31757987+okykputra@users.noreply.github.com>
2017-10-07 11:03:41 +0700

    Menghilangkan gears

--- resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/mainheader.blade.php
@@ -64,11 +64,6 @@
                         </ul>
                     </li>
                 @endif
-
-                <!-- Control Sidebar Toggle Button -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                </li>
             </ul>
         </div>
     </nav>
